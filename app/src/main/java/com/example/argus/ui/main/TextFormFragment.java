e7934e4fa302820f897fae7729af053eb2a2db89
Germain RULLIER <grullier@free.fr>
2021-11-12 23:08:03 +0100

    I'm an artist €-={°c°}=-$

--- app/src/main/java/com/example/argus/ui/main/TextFormFragment.java
+++ app/src/main/java/com/example/argus/ui/main/TextFormFragment.java
@@ -1,6 +1,10 @@
 package com.example.argus.ui.main;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -8,6 +12,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -33,11 +38,14 @@ public class TextFormFragment extends Fragment {
     private PageViewModel pageViewModel;
     private FragmentTextFormBinding binding;
     private int textColor = 0;
+    private int backgroundColor = 0;
+    private String text = "";
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putInt("textColor", this.textColor);
+        outState.putInt("backgroundColor", this.backgroundColor);
     }
 
     @Override
@@ -45,6 +53,7 @@ public class TextFormFragment extends Fragment {
         super.onCreate(savedInstanceState);
         if(savedInstanceState != null) {
             this.textColor = savedInstanceState.getInt("textColor");
+            this.backgroundColor = savedInstanceState.getInt("backgroundColor");
         }
         pageViewModel = new ViewModelProvider(this).get(PageViewModel.class);
     }
@@ -68,17 +77,27 @@ public class TextFormFragment extends Fragment {
             @Override
             public void onTextChanged(CharSequence s, int start,
                                       int before, int count) {
-                if(s.length() != 0)
-                    binding.textPreview.setText(s);
+                if(s.length() != 0) {
+                    text = s.toString();
+                    refreshTextBitmapPreview();
+                }
             }
         });
         binding.button2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                openColorPicker();
+                openTextColorPicker();
+            }
+        });
+        binding.button3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                openBackgroundColorPicker();
             }
         });
         if(textColor != 0) {
-            setTextAndButtonColor(textColor);
+            binding.button2.setBackgroundColor(textColor);
+        }
+        if(backgroundColor != 0) {
+            binding.button3.setBackgroundColor(backgroundColor);
         }
         View root = binding.getRoot();
         return root;
@@ -90,38 +109,68 @@ public class TextFormFragment extends Fragment {
         binding = null;
     }
 
-    public void openColorPicker(){
+    public void openTextColorPicker(){
         ColorPicker colorPicker = new ColorPicker(getActivity());
         colorPicker.setTitle(getString(R.string.tab_text_type_select_color_title));
-        /*
-        ArrayList<String> colors = new ArrayList<>();
-        colors.add("#82B926");
-        colors.add("#a276eb");
-        colors.add("#6a3ab2");
-        colors.add("#666666");
-        colors.add("#FFFF00");
-        colors.add("#3C8D2F");
-        colors.add("#FA9F00");
-        colors.add("#FF0000");
-        colorPicker.setColors(colors);
-         */
         colorPicker.show();
         colorPicker.setOnChooseColorListener(new ColorPicker.OnChooseColorListener() {
             @Override
             public void onChooseColor(int position,int color) {
                 textColor = color;
-                setTextAndButtonColor(color);
+                binding.button2.setBackgroundColor(color);
+                refreshTextBitmapPreview();
             }
-
             @Override
-            public void onCancel(){
-
-            }
+            public void onCancel(){}
         });
     }
 
-    public void setTextAndButtonColor(int color) {
-        binding.button2.setBackgroundColor(color);
-        binding.textPreview.setTextColor(color);
+    public void openBackgroundColorPicker(){
+        ColorPicker colorPicker = new ColorPicker(getActivity());
+        colorPicker.setTitle(getString(R.string.tab_text_type_select_color_title));
+        colorPicker.show();
+        colorPicker.setOnChooseColorListener(new ColorPicker.OnChooseColorListener() {
+            @Override
+            public void onChooseColor(int position,int color) {
+                backgroundColor = color;
+                binding.button3.setBackgroundColor(color);
+                refreshTextBitmapPreview();
+            }
+            @Override
+            public void onCancel(){}
+        });
+    }
+
+    private void refreshTextBitmapPreview() {
+        Bitmap b = generateTextBitmap();
+        binding.textPreview.setImageBitmap(b);
+    }
+
+    private Bitmap generateTextBitmap() {
+        Bitmap bitmap = Bitmap.createBitmap(200, 200, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+
+        // BG
+        Paint paintBg = new Paint();
+        paintBg.setColor(this.backgroundColor);
+        canvas.drawRect(0, 0, 200, 200, paintBg);
+
+        String string = text;
+        float scale = getResources().getDisplayMetrics().density;
+        Paint paintText = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paintText.setColor(this.textColor);
+        paintText.setTextSize((int) (14 * scale));
+        paintText.setShadowLayer(1f, 0f, 1f, Color.WHITE);
+
+        // draw text in the center
+        Rect bounds = new Rect();
+        paintText.getTextBounds(string, 0, string.length(), bounds);
+
+        int x = (bitmap.getWidth() - bounds.width())/2;
+        int y = (bitmap.getHeight() + bounds.height())/2;
+
+        canvas.drawText(string, x, y, paintText);
+
+        return bitmap;
     }
 }
