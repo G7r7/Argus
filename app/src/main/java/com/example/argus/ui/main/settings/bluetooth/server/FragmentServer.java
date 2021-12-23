ec4037422f157ce1cbc2ad31e8e7785cf7a9ddda
Germain RULLIER <grullier@free.fr>
2021-12-23 13:11:59 +0100

    Server Status update

--- app/src/main/java/com/example/argus/ui/main/settings/bluetooth/server/FragmentServer.java
+++ app/src/main/java/com/example/argus/ui/main/settings/bluetooth/server/FragmentServer.java
@@ -11,22 +11,26 @@ import androidx.fragment.app.Fragment;
 
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.argus.MainActivity;
+import com.example.argus.backend.client.BluetoothClientThread;
 import com.example.argus.backend.server.BluetoothServerThread;
 import com.example.argus.databinding.FragmentServerBinding;
 import com.google.android.material.snackbar.Snackbar;
 
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.UUID;
 
 public class FragmentServer extends Fragment {
 
+    private static final String TAG = "TAG";
     private ServerViewModel mViewModel;
     private FragmentServerBinding binding;
-    private BluetoothServerThread serverThread = null;
     UUID baseUUID = UUID.fromString("00000000-0000-1000-7007-00805F9B34FB");
 
     private Handler serverHandler = new Handler(){
@@ -43,27 +47,82 @@ public class FragmentServer extends Fragment {
         return new FragmentServer();
     }
 
+    private void updateServerStatus() {
+        if(((MainActivity)getActivity()).settings.getServerThread() == null)
+            binding.status.setText("Serveur arrêté");
+        else {
+            if (((MainActivity)getActivity()).settings.getServerThread().getState() == Thread.State.TERMINATED)
+                binding.status.setText("Serveur arrêté");
+            if (((MainActivity)getActivity()).settings.getServerThread().getState() == Thread.State.NEW)
+                binding.status.setText("Serveur crée");
+            if (((MainActivity)getActivity()).settings.getServerThread().getState() == Thread.State.RUNNABLE) {
+                binding.status.setText("Serveur démarré");
+                binding.log.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         // Inflate layout
         binding = FragmentServerBinding.inflate(inflater);
+        // Setting view corresponding to current thread state
+        updateServerStatus();
         // return root
         return binding.getRoot();
     }
 
     public void stopServer() {
-        serverThread.cancel();
+        if(((MainActivity) getActivity()).settings.getServerThread() != null)
+            try {
+                ((MainActivity) getActivity()).settings.getServerThread().cancel();
+            } catch (Exception e) {
+                Snackbar.make(getView(), "Erreur arrêt thread serveur", Snackbar.LENGTH_LONG).show();
+                Log.e(TAG, "startServer: ", e);
+                return;
+            }
         Snackbar.make(getView(), "Serveur arrêté", Snackbar.LENGTH_LONG).show();
         binding.log.setText("");
         binding.log.setVisibility(View.GONE);
+        updateServerStatus();
     }
 
     public void startServer() {
-        serverThread = new BluetoothServerThread(serverHandler, BluetoothAdapter.getDefaultAdapter(), baseUUID);
-        serverThread.start();
+        if(((MainActivity) getActivity()).settings.getServerThread() != null)
+            try {
+                ((MainActivity) getActivity()).settings.getServerThread().cancel();
+            } catch (Exception e) {
+                Snackbar.make(getView(), "Erreur arrêt thread serveur", Snackbar.LENGTH_LONG).show();
+                Log.e(TAG, "startServer: ", e);
+                return;
+            }
+
+        try {
+            HashMap<String, Object> settings = ((MainActivity) getActivity()).settings.getSettings();
+            BluetoothServerThread serverThread = new BluetoothServerThread(serverHandler, BluetoothAdapter.getDefaultAdapter(), baseUUID);
+            settings.put("serverThread", serverThread);
+            ((MainActivity)getActivity()).settings.updateSettings(settings);
+        } catch (Exception e) {
+            Snackbar.make(getView(), "Erreur création thread serveur", Snackbar.LENGTH_LONG).show();
+            Log.e(TAG, "startServer: ", e);
+            return;
+        }
+
+        try {
+            ((MainActivity)getActivity()).settings.getServerThread().start();
+            Snackbar.make(getView(),
+                    "Démarrage du thread serveur",
+                    Snackbar.LENGTH_LONG).show();
+        } catch (Exception e) {
+            Snackbar.make(getView(), "Erreur démarrage thread serveur", Snackbar.LENGTH_LONG).show();
+            Log.e(TAG, "connectToServer: ", e);
+            return;
+        }
+
         Snackbar.make(getView(), "Serveur démarré", Snackbar.LENGTH_LONG).show();
         binding.log.setVisibility(View.VISIBLE);
+        updateServerStatus();
     }
 
     @Override
